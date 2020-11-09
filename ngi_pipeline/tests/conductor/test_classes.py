88e93536f1af8f0f089d24c7628ac007a5e51258
Sara Sjunnebo <sara.sjunnebo@gmail.com>
2020-11-09 14:03:26 +0100

    Add tests for conductor/classes.py

--- ngi_pipeline/tests/conductor/test_classes.py
+++ ngi_pipeline/tests/conductor/test_classes.py
@@ -1,9 +1,8 @@
 import unittest
 import mock
 from ngi_pipeline.conductor.classes import NGIAnalysis, NGIObject, \
-                                            NGIProject, NGISample, \
-                                                NGILibraryPrep, NGISeqRun, \
-                                                    get_engine_for_bp, load_engine_module
+                                            NGIProject, NGISeqRun, \
+                                                get_engine_for_bp, load_engine_module
 
 class TestNGIAnalysis(unittest.TestCase):
 
@@ -33,7 +32,26 @@ class TestNGIProject(unittest.TestCase):
         proj2 = NGIProject('S.One_20_01', 'dir_P123', 'P123', '/some/path')
         self.assertEqual(proj1, proj2)
 
-class TestNGISample(unittest.TestCase):
+class TestHelperFunctions(unittest.TestCase):
 
-    def test_ngi_sample(self):
-        pass
+    @mock.patch('ngi_pipeline.conductor.classes.CharonSession.project_get')
+    @mock.patch('ngi_pipeline.conductor.classes.load_engine_module')
+    def test_get_engine_for_bp(self, mock_load, mock_charon):
+        mock_charon.return_value = {'best_practice_analysis': 'some_BP'}
+        mock_load.return_value = 'some_engine'
+        conf = {'dummy': 'conf'}
+        project = NGIProject('S.One_20_01', 'dir_P123', 'P123', '/some/path')
+        got_engine = get_engine_for_bp(project, config=conf)
+
+        mock_load.assert_called_once_with('some_BP', conf)
+        self.assertEqual(got_engine, 'some_engine')
+
+    @mock.patch('ngi_pipeline.conductor.classes.importlib.import_module')
+    def test_load_engine_module(self, mock_import):
+        mock_import.return_value = 'some_engine'
+        BP_analysis = 'some_BP'
+        conf = {'analysis': {'best_practice_analysis': {'some_BP': {'analysis_engine': 'some_engine'}}}}
+        got_engine = load_engine_module(BP_analysis, conf)
+        
+        mock_import.assert_called_once_with('some_engine')
+        self.assertEqual(got_engine, 'some_engine')
